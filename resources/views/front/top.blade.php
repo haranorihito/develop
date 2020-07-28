29d8383c48f59df0bd8a1eeeb8cf89f47f5b3f39
”haranorihito” <1721167@gmail.com>
2020-07-28 12:19:51 +0000

    fix

--- resources/views/front/top.blade.php
+++ resources/views/front/top.blade.php
@@ -15,14 +15,32 @@
                 </div>
             </div>
         </div>
-        <div class="links">
-            <a href="https://laravel.com/docs">Docs</a>
-            <a href="https://laracasts.com">Laracasts</a>
-            <a href="https://laravel-news.com">News</a>
-            <a href="https://blog.laravel.com">Blog</a>
-            <a href="https://nova.laravel.com">Nova</a>
-            <a href="https://forge.laravel.com">Forge</a>
-            <a href="https://github.com/laravel/laravel">GitHub</a>
+        <div class="top_second">
+            <h2>新メニュー登場！！</h2>
+            <hr>
+            <!--カラムを中央寄せにしてます。-->
+            <div class="row new_topic justify-content-center">
+                <div class="col-md-2">
+                    <img src="{{ asset('img/ramen1.jpg') }}" width="400vw" height="330vw">
+                </div>
+                <div class="col-md-3 new_topic_text">
+                    <h3><span class="under2">厚木家/ネギチャーシュー麺</span></h3>
+                    <p>歯応えの良いネギが濃厚な家系スープと相性抜群！<br>細切りにされたチャーシューもボリューム満点に入ってます！</p>
+                </div>
+            </div>
+        </div>
+        <!--仕切りの線を記載してます。-->
+        <div class="separate">
+            <hr>
+        </div>
+        <div class="row new_topic justify-content-center">
+            <div class="col-md-2">
+                <img src="{{ asset('img/motosuke.don.jpg') }}" width="400vw" height="330vw">
+            </div>
+            <div class="col-md-3 new_topic_text">
+                <h3><span class="under2">めん屋元助/チャーシュー丼</span></h3>
+                <p>口の中でホロホロとほぐれる柔らかチャーシューでご飯が進む！<br>腹ぺこの際には是非ラーメンのお供に！</p>
+            </div>
         </div>
     </div>
 @endsection
